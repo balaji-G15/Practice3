8a337b0bf26e1591a18097c8ce0120a439aafe0e
jadhavbalaji123@gmail.com <jadhavbalaji123@gmail.com>
2020-08-21 10:35:03 +0530

    Add  model person and some properties

---
+++ b-11Practice4/Practice4/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice4.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Nmae { get; set; }
+    }
+}
